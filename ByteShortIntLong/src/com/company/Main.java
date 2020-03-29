ab3cf80872e231c8ad7bab92c5e77e1faac4bee1
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-28 20:28:41 -0500

    byte primitive type in java

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -13,5 +13,13 @@ public class Main {
         System.out.println("Busted Min value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum value = " + myMinByteValue);
+        System.out.println("Byte Maximum value = " + myMaxByteValue);
+        System.out.println("Busted Max value = " + (myMaxByteValue + 1));
+        System.out.println("Busted Min value = " + (myMinByteValue - 1));
+
     }
 }
