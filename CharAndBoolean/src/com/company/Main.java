f829e2333fa993a9e7aeb73b3b23fd298951a3e7
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-29 11:19:57 -0500

    char and boolean primitive types in Java

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // for ex last key pressed by a user in the game, one char
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myUnicodeCharTwo = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeCharTwo);
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
