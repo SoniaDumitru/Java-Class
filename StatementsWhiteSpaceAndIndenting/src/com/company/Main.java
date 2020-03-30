5ab36889acd4450ec74b5a1c478935a4c62ed204
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-30 12:05:57 -0500

    what are statements, indentation and how java ignores white spacing

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Statements
+        int myVar = 50;
+        myVar++;
+        myVar--;
+
+        System.out.println("This is a test");
+        System.out.println("This is " +
+                "another " +
+                "test");
+
+        // Java ignores white spaces
+        int number    =    100;
+        System.out.println(number);
+
+        // Indentation makes code easier to read and to see the logical flow.
+    }
+}
