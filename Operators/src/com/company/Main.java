cc1c87b06995b517c6eead89b13a58c4dd4a2181
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-29 22:13:45 -0500

    more with java operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -23,5 +23,25 @@ public class Main {
         // modulus or remainder
         result = result % 3; // ( 4 % 3 = 1)
         System.out.println(result);
+
+        result++;
+        System.out.println(result);
+
+        result--;
+        System.out.println(result);
+
+        result += 2;
+        System.out.println(result);
+
+        // result = result * 10;
+        result *= 10;
+        System.out.println(result);
+
+        result /= 3;
+        System.out.println(result);
+
+        result -= 2;
+        System.out.println(result);
+
     }
 }
