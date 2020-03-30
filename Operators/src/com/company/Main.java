61520e5a5c52ec5380eda977aebf4047bee79ee8
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-29 22:09:30 -0500

    operators in java including modulus

---
+++ Operators/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println(result);
+
+        int previousResult = result;
+        System.out.println(previousResult);
+
+        result = result -1; // 3 - 1 = 2
+
+        System.out.println(result);
+        System.out.println(previousResult);
+
+        result = result * 10; // 2 * 10 = 20
+        System.out.println(result);
+
+        result = result / 5;
+        System.out.println(result);
+
+        // modulus or remainder
+        result = result % 3; // ( 4 % 3 = 1)
+        System.out.println(result);
+    }
+}
