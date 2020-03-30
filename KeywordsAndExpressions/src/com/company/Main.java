027abe3dfb6470d941208545a9e207528bea5e84
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-30 10:45:42 -0500

    which are expressions in java

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 km
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // Expressions challenge
+        int score = 100; // expression: score = 100
+        if (score > 99) {
+            System.out.println("You got high score!"); // expression "You got high score!"
+            score = 0; // expression: score = 0
+        }
+    }
+}
