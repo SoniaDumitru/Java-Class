a3fdaa463d53ec4fade0473b2f2639c1c90359ce
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-03 23:26:49 -0500

    positive and negative function

---
+++ PositiveNegativeZero/src/PositiveNegative.java
@@ -0,0 +1,14 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        checkNumber(10);
+    }
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if (number == 0) {
+            System.out.println("The number was zero");
+        }
+    }
+}
