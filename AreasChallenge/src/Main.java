55e46fa66245e903796f49cc853f43bb89e2de16
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-09 16:08:28 -0500

    calculated area for a circle and rectangle

---
+++ AreasChallenge/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(20));
+        System.out.println(AreaCalculator.area(20, 67));
+
+    }
+
+}
