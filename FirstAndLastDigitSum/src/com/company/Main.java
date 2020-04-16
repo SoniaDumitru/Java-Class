6dba4c1b89e1f12365cba1a53bb4acc5720f1561
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-16 17:02:25 -0500

    summing last digit and first digit in a number

---
+++ FirstAndLastDigitSum/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(1));
+        System.out.println(sumFirstAndLastDigit(10));
+        System.out.println(sumFirstAndLastDigit(109));
+
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int lastDigit = (number % 10);
+        while (number >= 10) {
+            number = number / 10;
+        }
+        return lastDigit + number;
+    }
+}
