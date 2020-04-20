dd308e779de7013272f7bc2b3926b931d1577847
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-20 11:18:31 -0500

    getting year input from user and calculate age

--- ReadingUserInput/src/com/company/Main.java
+++ ReadingUserInput/src/com/company/Main.java
@@ -6,9 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
 	    Scanner scanner = new Scanner(System.in); // Scanner Class
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // handle the problem with Enter Key
+        int age = 2018 - yearOfBirth;
+
         System.out.println("Enter your name: " );
         String name = scanner.nextLine(); // read a line from the input with .nextLine()
-        System.out.println(" Your name is " + name); // retrieve the info from the console
+
+
+        System.out.println(" Your name is " + name + " and you are " + age + " years old"); // retrieve the info from the console
 
         scanner.close(); // close the scanner to clear the memory
     }
