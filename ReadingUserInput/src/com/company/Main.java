79f877d80a5ded0b85b91c99ca50412c6675b363
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-20 11:45:27 -0500

    called .hasNextInt() method to test for user input

--- ReadingUserInput/src/com/company/Main.java
+++ ReadingUserInput/src/com/company/Main.java
@@ -8,16 +8,24 @@ public class Main {
 	    Scanner scanner = new Scanner(System.in); // Scanner Class
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle the problem with Enter Key
-        int age = 2018 - yearOfBirth;
 
-        System.out.println("Enter your name: " );
-        String name = scanner.nextLine(); // read a line from the input with .nextLine()
+        boolean hasNextInt = scanner.hasNextInt(); // if the input is not a number will return false
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle the problem with Enter Key
 
+            System.out.println("Enter your name: " );
+            String name = scanner.nextLine(); // read a line from the input with .nextLine()
 
-        System.out.println(" Your name is " + name + " and you are " + age + " years old"); // retrieve the info from the console
-
+            int age = 2020 - yearOfBirth;
+            if (age >= 0 && age <= 100) {
+                System.out.println(" Your name is " + name + " and you are " + age + " years old"); // retrieve the info from the console
+            } else {
+                System.out.println("Invalid year of birth!");
+            }
+        } else {
+            System.out.println("Invalid Year Value!");
+        }
         scanner.close(); // close the scanner to clear the memory
     }
 }
