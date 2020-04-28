aaf7b251ea65bc503bbf6eb0a753e9a270e5306a
Sonia Dumitru <soni.dumitru@gmail.com>
2020-04-28 11:51:11 -0500

    inheritance challenge, vehicle/car/toyota

---
+++ 4_OOPClassesConstructorsAndInheritance/InheritanceChallenge/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota(36);
+        toyota.accelerate(30);
+        toyota.steer(20);
+    }
+}
